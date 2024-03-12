ac07478b799c7b7836b5f0e1032adc260d07ef2a
devYule <privateDevYule@gmail.com>
2024-03-12 21:53:31 +0900

    2024-03-12 --by Hyunmin

---
+++ src/components/Sketchbook.jsx
@@ -0,0 +1,15 @@
+const containerStyle = {
+    width: "100%",
+    padding: "16px",
+};
+
+const textStyle = {
+    color: "blue",
+    textAlign: "center",
+};
+
+return (
+    <div style={containerStyle}>
+        <p style={textStyle}>Hello world!</p>
+    </div>
+)
